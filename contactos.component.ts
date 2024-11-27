a0688f87aee6b04c7407bd8aa1472decc800ac80
mar1511 <marciamirandapinto15152002@gmail.com>
2024-11-26 23:34:43 -0400

    Add files via upload

---
+++ contactos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactos', // El selector debe coincidir con el usado en app.component.html
+  templateUrl: './contactos.component.html',
+  styleUrls: ['./contactos.component.css']
+})
+export class ContactosComponent {}
