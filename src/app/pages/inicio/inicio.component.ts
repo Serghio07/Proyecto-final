e3e6c75efb687ccf8e03612bf61d5e1d240bbdcb
root <root@DESKTOP-KEC5OF3>
2024-11-28 03:20:22 -0400

    home done

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+  onAction() {
+    // Muestra un mensaje o redirige a otra página
+    alert('¡Explora nuestra tienda en Llamitas Sport!');
+    // Ejemplo de redirección si usas Angular Router:
+    // this.router.navigate(['/tienda']);
+  }
 }
+
